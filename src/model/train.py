0c9c404281864fef0ac7b78a8f0f48acb37da0ce
tasosptl <53862819+tasosptl@users.noreply.github.com>
2023-12-18 20:32:54 +0000

    Added split data

--- src/model/train.py
+++ src/model/train.py
@@ -6,6 +6,8 @@ import os
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 from sklearn.linear_model import LogisticRegression
 
 
@@ -33,7 +35,15 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-def split_data(df):
+def split_data(df, test_size=0.2, random_state=42):
+    # split features and target
+    X = df.drop('target', axis=1)
+    y = df['target']
+
+    # split data into training and testing set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+    return X_train, X_test, y_train, y_test
     
 
 
